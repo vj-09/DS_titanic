5cffb6aab756bc218022865367288780d1f956c3
vj-09 <athithyavijay@gmail.com>
2017-10-09 22:56:18 +0530

    added files

---
+++ age_fill.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import numpy as np
+dataset = pd.read_csv('dataset/train.csv')
+print(dataset.head(n=5))
+sum1,count=0,0
+for a in dataset['Age']:
+    if  np.isnan(a):
+        continue
+    else:
+        count = count +1
+        sum1 = sum1 +a
+        #print sum1
+mean = sum1 / count
+print mean
+for a in dataset['Age']:
+    if np.isnan(a):
+        dataset[[a]]= mean
